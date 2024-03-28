4e3359f536e12702849a51371fd7f1c1ff32cab8
ange1castillo <angelac1357@gmail.com>
2024-03-28 05:21:59 -0700

    Hangman: needs refactoring

--- Tasks/Hangman/main.cpp
+++ Tasks/Hangman/main.cpp
@@ -2,7 +2,6 @@
 
 #include "Random.h"
 
-
 namespace WordList{
     std::vector<std::string> wordsList{"mystery", "broccoli", "account", "almost", "spaghetti", "opinion", "beautiful", "distance", "luggage"};
 
@@ -13,14 +12,36 @@ namespace WordList{
 
 class Session{
     private:
+        int m_guessesLeft {6};
         std::string_view m_word {WordList::getRandomWord()};
         std::vector<bool> m_lettersGuessed {std::vector<bool>(26)}; 
         std::size_t toIndex(char guess) const {return static_cast<std::size_t>((guess % 32) - 1);}  
 
     public:
+        int getGuessesLeft() const {return m_guessesLeft;}
+        void setGuessesLeft(int guesses) {m_guessesLeft -= guesses;}
         std::string_view getWord() const {return m_word;}
+        std::vector<bool> getLettersGuessed() const {return m_lettersGuessed;}
         bool isLetterGuessed(char guess) const {return m_lettersGuessed[toIndex(guess)];}
         void setLetterGuessed(char guess){ m_lettersGuessed[toIndex(guess)] = true;}
+        bool isLetterInWord(char guess) const {
+            for(const auto& character: m_word){
+                if(guess == character){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool isWon(){
+            for(const auto& character: m_word){
+                if(!isLetterGuessed(character)){
+                    return false;
+                }
+            }
+
+            return true;
+        }
 };
 
 void displayWord(const Session& game){
@@ -31,13 +52,26 @@ void displayWord(const Session& game){
         }
 
         else{
-            std::cout << '_';
+            std::cout << '_'; 
+        }
+    }
+}
+
+void displayWrongGuesses(const Session& game){
+    std::cout << " Wrong guesses: ";
+    for(int guessesLeft {0}; guessesLeft <game.getGuessesLeft(); ++guessesLeft){
+        std::cout << '+';
+    }
+
+    for(char c = 'a'; c <= 'z'; ++c){
+        if(game.isLetterGuessed(c) && !game.isLetterInWord(c)){
+            std::cout << c;
         }
     }
     std::cout << std::endl;
 }
 
-char getGuess(){
+char getGuess(const Session& game){
     while(true){
         std::cout << "Enter your next letter: ";
         char guess {};
@@ -56,22 +90,51 @@ char getGuess(){
             continue;
         }
 
+        if(game.isLetterGuessed(guess)){
+            std::cout << "You already guessed " << guess << ". Try again." << std::endl;
+            continue;
+        }
+
         return guess;
     }            
 }
 
+void endGame(const Session& game){
+    displayWord(game);
+    displayWrongGuesses(game);
+    if(!game.getGuessesLeft()){
+        std::cout << "You lost! The word was: " << game.getWord() << std::endl;
+    }
+    else{
+        std::cout << "You won!" << std::endl;
+    }
+}
+
+void check(Session& game, char guess){
+    if(game.isLetterInWord(guess)){
+        std::cout << "Yes! '" << guess << "' is in the word!" << std::endl << std::endl;
+    }
+
+    else{
+        std::cout << "No, '" << guess << "' is not in the word!" << std::endl << std::endl;
+        game.setGuessesLeft(1);
+    }
+}
+
 void playGame(Session& game){
-    int guesses {6};
+    int guesses {game.getGuessesLeft()};
 
-    while(--guesses){
+    while(game.getGuessesLeft() && !game.isWon()){
         displayWord(game);
+        displayWrongGuesses(game);
 
-        char guess {getGuess()};
-
-        std::cout << "You entered: " << guess << std::endl;
+        char guess {getGuess(game)};
 
         game.setLetterGuessed(guess);
+
+        check(game, guess);
     }
+    endGame(game);
 }
 
 void start(){
